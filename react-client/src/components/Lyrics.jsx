de282f9e2e44f1e21a4818b332372138db65db89
Edward Kim <mr.edward.kim@gmail.com>
2017-05-18 19:33:12 -0700

    Add english translation support for foreign language songs

--- react-client/src/components/Lyrics.jsx
+++ react-client/src/components/Lyrics.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import renderif from 'render-if';
 import Player from './Player.jsx';
 
 class Lyrics extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: ' ',
-      artist: ' '
+      title: '',
+      artist: '',
+      langOriginal: true
     };
+    this.toggleLang = this.toggleLang.bind(this);
+  }
+
+  toggleLang () {
+    console.log ('toggling');
+    this.setState({langOriginal: !this.state.langOriginal});
   }
 
   render() {
@@ -20,11 +28,22 @@ class Lyrics extends React.Component {
     } else {
       return (
         <div className="lyrics" >
-          <h6>{this.props.songNameAndArtist[0] + ' - ' + this.props.songNameAndArtist[1]}</h6>
-          {this.props.showPlayer ?
-            <Player spotifyURI={this.props.spotifyURI} loading={this.props.loading}/>
-          : null }
-          <pre>{this.props.lyrics}</pre>
+          {renderif(this.props.lyricsLang !== 'en')(
+            <button className="submitbutton floatRight" onClick={this.toggleLang}>
+              {this.state.langOriginal ? 'en' : this.props.lyricsLang}
+            </button>
+          )}
+          {this.state.langOriginal
+            ? <h6>{this.props.songNameAndArtist[0] + ' - ' + this.props.songNameAndArtist[1]}</h6>
+            : <h6>{this.props.artistEnglish + ' - ' + this.props.titleEnglish}</h6>
+          }
+          {this.props.showPlayer
+            ? <Player spotifyURI={this.props.spotifyURI} loading={this.props.loading}/>
+            : null }
+          {this.state.langOriginal
+            ? <pre>{this.props.lyrics}</pre>
+            : <pre>{this.props.lyricsEnglish}</pre>            
+          }
         </div>
       );
     }
