83a67d86d317a3b96571aa002853b12518ef8c6d
Edward Kim <mr.edward.kim@gmail.com>
2017-05-18 19:38:04 -0700

    Formatting changes

--- react-client/src/components/Search.jsx
+++ react-client/src/components/Search.jsx
@@ -32,22 +32,24 @@ class Search extends React.Component {
   render() {
     return (
       <div>
-      <div className="search">
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="title" value={this.state.title} placeholder="title" onChange={this.handleTitleChange} className="textbox"/>
-          <input type="text" name="artist" value={this.state.artist} placeholder="artist" onChange={this.handleArtistChange} className="textbox"/>
-          <input type="submit" value="Search" className="submitbutton"/>
+        <div className="search">
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" name="title" value={this.state.title} placeholder="title" onChange={this.handleTitleChange} className="textbox"/>
+            <input type="text" name="artist" value={this.state.artist} placeholder="artist" onChange={this.handleArtistChange} className="textbox"/>
+            <input type="submit" value="Search" className="submitbutton"/>
           </form>
           <form>
-          {this.props.showPrev ?
-            <div className="resultsBar" onClick={this.prevResults} >
-            <div className="searchHeadline">Search Results</div>
-            {this.props.upDown ?
-            <img className="searchPrevUp" src="./img/ic_down.svg" width="18" height="18"/>
-            : <img className="searchPrevDown" src="./img/ic_up.svg" width="18" height="18"/>}
-            </div>
-            : null}
-        </form>
+            {this.props.showPrev
+              ? <div className="resultsBar" onClick={this.prevResults} >
+                  <div className="searchHeadline">Search Results</div>
+                  {this.props.upDown
+                    ? <img className="searchPrevUp" src="./img/ic_down.svg" width="18" height="18"/>
+                    : <img className="searchPrevDown" src="./img/ic_up.svg" width="18" height="18"/>
+                  }
+                </div>
+              : null
+            }
+          </form>
         </div>
       </div>
     );
