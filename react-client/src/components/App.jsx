49478c9550aaeadaca9fc2f5f0f5bbf7069879a0
Michael Diodoro <michael@Michaels-MacBook-Pro.local>
2017-05-19 14:54:44 -0700

    fixing playlist

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -47,7 +47,8 @@ class App extends React.Component {
       searchResultsLoadingUser: false,
       playlists: null,
       currentPlaylist: null,
-      playlistList: []
+      playlistList: [],
+      currentPlaylist: null
     };
     this.search = this.search.bind(this);
     this.process = this.process.bind(this);
@@ -242,6 +243,7 @@ class App extends React.Component {
                 />
               : null
             }
+
             {this.state.showPlayer
               ? <Lyrics
                   showPlayer={this.state.showPlayer}
@@ -280,7 +282,6 @@ class App extends React.Component {
             {this.state.showStats ?
             <Stats/> : null
             }
-
             <PlaylistEntry playlistList={this.state.playlistList} addToPlaylist={this.state.addToPlaylist} createNewPlaylists={this.createNewPlaylists} setCurrentPlaylist={this.setCurrentPlaylist} currentPlaylist={this.state.currentPlaylist} playlists={this.state.playlists} />
           </div>
             <User showPrev={this.state.showResultsUser} prev={this.showResultsUser} upDown={this.state.upDownUser} runUpDown={this.upDownUser} process={this.process} searchResultsLoading={this.state.searchResultsLoadingUser} loadPastSearchResults={this.loadPastSearchResults}/> {this.state.showMood
