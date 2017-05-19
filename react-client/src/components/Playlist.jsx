b4c214897b3243d41ee559b6d5296c610ee20ca2
Michael Diodoro <michael@Michaels-MacBook-Pro.local>
2017-05-19 10:30:38 -0700

    Working on playlist

--- react-client/src/components/Playlist.jsx
+++ react-client/src/components/Playlist.jsx
@@ -3,35 +3,35 @@ import React from 'react';
 class Playlist extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   playlists: []
-    // }
+    this.state = {
+      playlists: []
+    }
   }
 
-  // updatePlaylist() {
-  //   if (this.state.playlists.length) {
-  //     let newPlaylists = this.playlists.slice();
-  //     newPlaylists.push(this.props.playlist);
-  //     this.setState({
-  //       playlists: newPlaylists
-  //     })  
-  //   } else {
-  //     let newPlaylists = [];
-  //     newPlaylists.push(this.props.playlists);
-  //     this.setState({
-  //       playlists: newPlaylists
-  //     })
-  //   }
-  // }
+  updatePlaylist() {
+    if (this.state.playlists.length) {
+      let newPlaylists = this.playlists.slice();
+      newPlaylists.push(this.props.playlist);
+      this.setState({
+        playlists: newPlaylists
+      })  
+    } else {
+      let newPlaylists = [];
+      newPlaylists.push(this.props.playlists);
+      this.setState({
+        playlists: newPlaylists
+      })
+    }
+  }
 
-  // render() {
-  //   this.updatePlaylist();
-  //   return (
-  //     <div>
-  //       <h2>{this.props.playlist}</h2>
-  //     </div>
-  //   )
-  // }
+  render() {
+    this.updatePlaylist();
+    return (
+      <div>
+        <h2>{this.props.playlist}</h2>
+      </div>
+    )
+  }
 }
 
 export default Playlist;
