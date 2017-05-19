b93d58add6a1333561c84641887822f8b2798462
Shawn Feng <shawnsfeng@gmail.com>
2017-05-18 20:05:16 -0700

    create Stats component

---
+++ react-client/src/components/Stats.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+class Stats extends Component {
+  render() {
+    return (
+      <div>
+        <P>hello from Stats component</P>
+        <Button class="btn">Click for Stats</Button>
+      </div>
+    );
+  }
+}
