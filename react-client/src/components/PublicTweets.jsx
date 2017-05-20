9598203096c144410e40ede65548df5a196b2c27
Mahima Srikanta <mahima.srikanta@gmail.com>
2017-05-20 10:49:56 -0700

    Public Tweets Setup

--- react-client/src/components/PublicTweets.jsx
+++ react-client/src/components/PublicTweets.jsx
@@ -10,25 +10,6 @@ class PublicTweets extends React.Component {
     super(props);
     this.state = {
       allTweets: [],
-      tweetData: {
-        id: 'XXX',
-        user: {
-          name: 'XXX',
-          screen_name: 'XXX',
-          profile_image_url: 'XXX'
-        },
-        text: 'XXX',
-        created_at: 'XXX',
-        favorite_count: 'XXX',
-        retweet_count: 'XXX',
-        entities: {
-          media: [],
-          urls: [],
-          user_mentions: [],
-          hashtags: [],
-          symbols: []
-        } 
-      }
     };
   } 
   componentDidMount() {
@@ -37,22 +18,42 @@ class PublicTweets extends React.Component {
       if (!res.data) {
         console.log('error');
       }
+      console.log(res.data.statuses);
       this.setState({allTweets: res.data.statuses});
-    });
-    
-    
-  }
+    });       
+  } 
 
-  
   render() {
     console.log(this.props.tweets);
     return (
       <div>
         <Header />
-          <h1> Hello from PublicTweets Page</h1>
-      </div>
-    );
+         {this.state.allTweets.map((tweet, i) => {                
+           var formattedObject = {
+             'id': tweet.id,
+             'user': {
+               'name': tweet.user.name,
+               'screen_name': tweet.user.screen_name,
+               'profile_image_url': tweet.user.profile_image_url,
+             },
+             'text': tweet.text,
+             'created_at': tweet.created_at,
+             'favorite_count': tweet.favorite_count,
+             'retweet_count': tweet.retweet_count,
+             'entities': {
+               'urls': tweet.entities.urls,
+               'user_mentions': tweet.entities.user_mentions,
+               'hashtags': tweet.entities.hashtags,
+               'symbols': tweet.entities.symbols,
+             }
+           };
+           return <Tweet data={formattedObject} key={i} />;
+         })}
+         </div>         
+    );     
   }
+   
+
 
 }
 export default PublicTweets;
