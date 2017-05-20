67f82c3beb327139b3907046a77a7552a155f6b2
Michael Diodoro <michael@Michaels-MacBook-Pro.local>
2017-05-20 14:03:43 -0700

    Playlist

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -248,36 +248,37 @@ class App extends React.Component {
           </div>
           <div className="col2">
             <div>
-            <PlaylistEntry 
-              playlistList={this.state.playlistList} 
-              addToPlaylist={this.state.addToPlaylist} 
-              createNewPlaylists={this.createNewPlaylists} 
-              setCurrentPlaylist={this.setCurrentPlaylist} 
-              currentPlaylist={this.state.currentPlaylist} 
-              playlists={this.state.playlists} 
-              currentPlaylist={this.state.currentPlaylist}
-              currentSongNameAndArtist={this.state.currentSongNameAndArtist}
-              process={this.process}
-            />
-            <User
-              showPrev={this.state.showResultsUser}
-              prev={this.showResultsUser}
-              upDown={this.state.upDownUser}
-              runUpDown={this.upDownUser}
-              process={this.process}
-              searchResultsLoading={this.state.searchResultsLoadingUser}
-              loadPastSearchResults={this.loadPastSearchResults}
-              showUserStats={this.showUserStats}
-            />
+              <PlaylistEntry 
+                playlistList={this.state.playlistList} 
+                addToPlaylist={this.state.addToPlaylist} 
+                createNewPlaylists={this.createNewPlaylists} 
+                setCurrentPlaylist={this.setCurrentPlaylist} 
+                currentPlaylist={this.state.currentPlaylist} 
+                playlists={this.state.playlists} 
+                currentPlaylist={this.state.currentPlaylist}
+                currentSongNameAndArtist={this.state.currentSongNameAndArtist}
+                process={this.process}
+              />
+              <User
+                showPrev={this.state.showResultsUser}
+                prev={this.showResultsUser}
+                upDown={this.state.upDownUser}
+                runUpDown={this.upDownUser}
+                process={this.process}
+                searchResultsLoading={this.state.searchResultsLoadingUser}
+                loadPastSearchResults={this.loadPastSearchResults}
+                showUserStats={this.showUserStats}
+              />
 
-            {this.state.showStats ?
-            <Stats/> : null
-            }
+              {this.state.showStats ?
+              <Stats/> : null
+              }
 
+            </div>
+              <User showPrev={this.state.showResultsUser} prev={this.showResultsUser} upDown={this.state.upDownUser} runUpDown={this.upDownUser} process={this.process} searchResultsLoading={this.state.searchResultsLoadingUser} loadPastSearchResults={this.loadPastSearchResults}/> {this.state.showMood
+                ? <Mood watson={this.state.watson} songNameAndArtist={this.state.currentSongNameAndArtist}/>
+                : null}
           </div>
-            <User showPrev={this.state.showResultsUser} prev={this.showResultsUser} upDown={this.state.upDownUser} runUpDown={this.upDownUser} process={this.process} searchResultsLoading={this.state.searchResultsLoadingUser} loadPastSearchResults={this.loadPastSearchResults}/> {this.state.showMood
-              ? <Mood watson={this.state.watson} songNameAndArtist={this.state.currentSongNameAndArtist}/>
-              : null}
         </div>
       </div>
     );
