8737d05bd7a23111250510f8faa24c9fd2ec17b9
Michael Diodoro <michael@Michaels-MacBook-Pro.local>
2017-05-19 17:53:12 -0700

    playlist

--- react-client/src/components/Search.jsx
+++ react-client/src/components/Search.jsx
@@ -33,6 +33,8 @@ class Search extends React.Component {
       artist: '', 
       showPrev: true
     });
+    //This is what I am currently working on
+    //Have to actually make the single song load istead of the roster of songs
   }
 
   prevResults(e) {
