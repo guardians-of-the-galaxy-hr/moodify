64dd408429f71219f018274b8e5a03ec872e547a
Michael Diodoro <michael@Michaels-MacBook-Pro.local>
2017-05-20 17:56:41 -0700

    Finished stuff

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -360,15 +360,7 @@ class App extends React.Component {
               {this.state.showStats ?
               <Stats/> : null
               }
-
-              <User showPrev={this.state.showResultsUser} 
-              prev={this.showResultsUser} 
-              upDown={this.state.upDownUser} 
-              runUpDown={this.upDownUser} 
-              process={this.process} 
-              searchResultsLoading={this.state.searchResultsLoadingUser} 
-              loadPastSearchResults={this.loadPastSearchResults}
-            /> {this.state.showMood
+                {this.state.showMood
                 ? <Mood 
                   watson={this.state.watson} 
                   songNameAndArtist={this.state.currentSongNameAndArtist}/>
