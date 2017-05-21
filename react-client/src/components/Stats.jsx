1419032f03a23592fbf919cf70590f295464d65b
Shawn Feng <shawnsfeng@gmail.com>
2017-05-21 16:33:43 -0700

    modify stats content

--- react-client/src/components/Stats.jsx
+++ react-client/src/components/Stats.jsx
@@ -7,12 +7,12 @@ class Stats extends React.Component {
   render() {
     return (
       <div>
-        <p>User Name: {this.props.userStatsInfo.username}</p>
-        <p>Total song has listened: {this.props.userStatsInfo.totalSongsListened}</p>
-        <p>List of songs that have been played: {this.props.userStatsInfo.listenedSongsList}</p>
+        <p>Logged in as: <span className="stats-span">{this.props.userStatsInfo.username}</span></p>
+        <p>Total songs listened: <span className="stats-span">{this.props.userStatsInfo.totalSongsListened}</span></p>
       </div>
     );
   }
 }
 
 export default Stats;
+// <p>List of songs that have been played: <span className="stats-span">{this.props.userStatsInfo.listenedSongsList}</span></p>
